09c73d8002087164b7b902be230b04f77b99ebd5
seungnyun <vas0707@naver.com>
2024-01-10 16:10:07 +0900

    first commit

---
+++ src/main/java/com/jang/mtg/MtRoomReservationBootJspApplication.java
@@ -0,0 +1,13 @@
+package com.jang.mtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtRoomReservationBootJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtRoomReservationBootJspApplication.class, args);
+	}
+
+}
